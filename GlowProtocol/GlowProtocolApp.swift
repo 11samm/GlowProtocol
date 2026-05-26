a8705622a2a002ee414ccf6a6b5d84780ade610a
11samm <11samuelgarcia@gmail.com>
2026-05-25 18:28:06 -0700

    Initial Commit

---
+++ GlowProtocol/GlowProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowProtocolApp.swift
+//  GlowProtocol
+//
+//  Created by Samuel Garcia on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct GlowProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
